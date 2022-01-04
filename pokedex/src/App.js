fa20e9c2052e09aba5cc6b240b04f05473214bd0
Anna Isabella Gomes Quaranta <bellaquaranta@gmail.com>
2022-01-04 16:25:05 -0300

    Requisições feitas

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,10 +1,56 @@
 import Router from "./routes/Router";
+import useRequestData from "./hooks/useRequestData";
+import UrlsContext from "./context/UrlsContext";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([])
+  const detalhesPokemon = []
+
+  useEffect(() => {
+    pegarPokemons()
+  }, [])
+
+  //PRIMEIRA REQUISIÇÃO - RETORNA ARRAY DE NOMES E URLS
+
+  const pegarPokemons = async () => {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=1")
+      setPokemons(response.data.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // PEGA DETALHES DOS POKEMONS 
+
+  const pegarDetalhesPokemons = async (url) => {
+    try{
+        const response = await axios.get(url)
+        detalhesPokemon.push(response.data)
+        // console.log(detalhesPokemon)
+
+    }catch(error){
+        // console.log(error.response)
+    }
+}
+  // SALVA ARRAY DE URLS
+  const urlsPokemons = pokemons && pokemons.map((pokemon) => {
+    return pokemon.url
+  })
+
+
+  //FAZ REQUISIÇÃO COM AS URLS E SALVA NUM ARRAY TODAS AS INFORMAÇÕES
+  urlsPokemons && urlsPokemons.map((url) => {
+    pegarDetalhesPokemons(url)
+  })
+
   return (
-    <div>
-      <Router/>
-    </div>
+    <UrlsContext.Provider value={detalhesPokemon}>
+      <Router />
+    </UrlsContext.Provider>
   );
 }
 
--- pokedex/src/pages/HomePage.js
+++ pokedex/src/pages/HomePage.js
@@ -1,7 +1,22 @@
+import { useContext } from "react"
+import UrlsContext from "../context/UrlsContext"
+
 const HomePage = () => {
-    return(
+    const detalhesPokemons = useContext(UrlsContext)
+    console.log(detalhesPokemons)
+
+    const CardPokemon = detalhesPokemons && detalhesPokemons.map((pokemon) => {
+        return (
+            <div>
+                <img src={pokemon.sprites.front_default} alt="foto do pokemon" />
+                <p>{pokemon.name}</p>
+            </div>
+        )
+    })
+
+    return (
         <div>
-            HomePage
+            {CardPokemon}
         </div>
     )
 }
